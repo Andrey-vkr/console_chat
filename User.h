32aa0225b75eed2cec7e74a98e50b386141b69b0
Andrey <and.vkr@mail.ru>
2024-01-24 18:38:13 +0300

    2_file

---
+++ User.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+#include<string>
+
+class User
+{
+	const std::string _login;
+	std::string _password;
+	std::string _name;
+
+public:
+	User(const std::string& login, const std::string& password, const std::string& name)
+		:_login(login), _password(password), _name(name) {}
+
+	const std::string& getLogin() const
+	{
+		return _login;
+	}
+
+	const std::string& getPassword() const
+	{
+		return _password;
+	}
+	void setPassword(const std::string& password)
+	{
+		_password = password;
+	}
+
+	const std::string& getName()const
+	{
+		return _name;
+	}
+	void setName(const std::string& name)
+	{
+		_name = name;
+	}
+};
+
+
+
+
+
+
+
+
