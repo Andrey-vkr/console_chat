5f607449c5613facb6572e626d35b9242d33ebd5
Andrey <and.vkr@mail.ru>
2024-01-24 18:36:43 +0300

    1_file

---
+++ main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include "Chat.h"
+
+
+int main()
+{
+	setlocale(LC_ALL, "");
+
+	Chat chat;
+
+	chat.begin();
+
+	while (chat.ChatWork())
+	{
+		chat.RegistrationShow();
+
+		while (chat.getActualUser())
+		{
+			chat.MessagShow();
+		}
+	}
+	return 0;
+}
+
+
+
